af525cf820655db8d0cbae89ea2319a1b8e02653
moshedruk <dmd6708713@gmail.com>
2024-10-10 12:37:38 +0300

    remove dist files

--- dist/interfaces/Icomment.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commentSchema = void 0;
-const mongoose_1 = require("mongoose");
-exports.commentSchema = new mongoose_1.Schema({});
