fd30cfd2b4810dbe94342b8f36b1c60bc58261e2
ruviRedner <r.y.r7966@gmail.com>
2024-10-10 16:35:44 +0300

    add logout and saving in localStorage

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import loginDTO from "../interfaces/Ilogin";
+import { loginToSystem, logoutFromSystem } from "../services/authService";
 
 
 export const login = async (
@@ -8,10 +9,19 @@ export const login = async (
   ): Promise<void> => {
     try {
       const token = await loginToSystem(req.body)
+      localStorage.setItem("token", token as string)
       res.status(200).json({ token });
 
     } catch (error) {
       res.status(500).json({ error });
     }
   };
-  
+
+  export const logout = async (req: Request, res: Response):Promise<void> => {
+    try {
+      await logoutFromSystem();
+      res.status(200).json({ message: "Logged out successfully" });
+    }catch (err) {
+    res.status(500).json({ err });
+  }
+}
