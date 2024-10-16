8277fb042cc0c1b0bae426efcd067eb5af44ffa2
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 09:19:42 +0300

    update

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,48 +1,61 @@
-import bcrypt from "bcrypt";
-import loginDTO from "../interfaces/Ilogin";
-import userModel from "../models/userModel";
-import jwt from "jsonwebtoken"
-import tokenPaloed from "../interfaces/tokenPayloed";
+import bcrypt from 'bcrypt';
+import loginDTO from '../interfaces/Ilogin';
+import userModel from '../models/userModel';
+import jwt from 'jsonwebtoken';
+import tokenPaloed from '../interfaces/tokenPayloed';
 
-export const loginToSystem = async (userData: loginDTO): Promise<Error | string> => {
-    const { username, password } = userData;
+export const loginToSystem = async (
+  userData: loginDTO
+): Promise<Error | string> => {
+  const { username, password } =
+    userData;
 
-    
-    const user = await userModel.findOne({ username});
+  const user = await userModel.findOne({
+    username
+  });
 
-    
-    if (!user) {
-        return new Error("User not found");
-    }
+  if (!user) {
+    return new Error('User not found');
+  }
 
-   
-    if (!password) {
-        throw new Error("Password is required");
-    }
+  if (!password) {
+    throw new Error(
+      'Password is required'
+    );
+  }
 
-   
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (!isPasswordMatch) {
-        throw new Error("403: Wrong password");
-    }
-    const Hpassword = await bcrypt.compare(password, user.password);
-    if (!Hpassword) {
-        throw new Error("403: Wrong password");
-    }
+  const isPasswordMatch =
+    await bcrypt.compare(
+      password,
+      user.password
+    );
+  if (!isPasswordMatch) {
+    throw new Error(
+      '403: Wrong password'
+    );
+  }
+  const Hpassword =
+    await bcrypt.compare(
+      password,
+      user.password
+    );
+  if (!Hpassword) {
+    throw new Error(
+      '403: Wrong password'
+    );
+  }
 
-    
-    const payload:tokenPaloed = {
-        id: user._id,
-        username: user.username,
-        email: user.email
-    };
+  const payload: tokenPaloed = {
+    id: user._id,
+    username: user.username,
+    email: user.email
+  };
 
-   
-    const token:string = jwt.sign(payload, process.env.TOKEN_SECRET as string, { expiresIn: "1h" });
-    
+  const token: string = jwt.sign(
+    payload,
+    process.env.TOKEN_SECRET as string,
+    { expiresIn: '1h' }
+  );
 
-    return token;
+  return token;
 };
-
-
-
