e7a454e87ed1fcdee6e895f152184b0bd3d19d6e
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 09:13:08 +0300

    delete note in hebrew and activate bcrypt

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -7,39 +7,39 @@ import tokenPaloed from "../interfaces/tokenPayloed";
 export const loginToSystem = async (userData: loginDTO): Promise<Error | string> => {
     const { username, password } = userData;
 
-    // מוצאים משתמש לפי שם משתמש
+    
     const user = await userModel.findOne({ username});
 
-    // אם המשתמש לא נמצא
+    
     if (!user) {
         return new Error("User not found");
     }
 
-    // אם לא נשלחה סיסמה
+   
     if (!password) {
         throw new Error("Password is required");
     }
 
-    // בדיקה אם הסיסמה תואמת את הסיסמה המוצפנת ב-Database
-    // const isPasswordMatch = await bcrypt.compare(password, user.password);
-    // if (!isPasswordMatch) {
-    //     throw new Error("403: Wrong password");
-    // }
-    // const Hpassword = await bcrypt.compare(password, user.password);
-    // if (!Hpassword) {
-    //     throw new Error("403: Wrong password");
-    // }
+   
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+        throw new Error("403: Wrong password");
+    }
+    const Hpassword = await bcrypt.compare(password, user.password);
+    if (!Hpassword) {
+        throw new Error("403: Wrong password");
+    }
 
-    // יצירת טוקן
+    
     const payload:tokenPaloed = {
         id: user._id,
         username: user.username,
         email: user.email
     };
 
-    console.log(process.env.TOKEN_SECRET); // בדוק שהסוד לא ריק
+   
     const token:string = jwt.sign(payload, process.env.TOKEN_SECRET as string, { expiresIn: "1h" });
-    console.log(token); // הדפס את הטוקן שנוצר
+    
 
     return token;
 };
