f27074615aa01e8d2c30b3575499a610914f2ba8
ruviRedner <r.y.r7966@gmail.com>
2024-10-16 09:20:44 +0300

    delete unused notes

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,167 +1,135 @@
-import mongoose from "mongoose";
-import { IComment } from "../interfaces/Icomment"
-import postModel from "../models/postModel"
-import userModel from "../models/userModel"
+import mongoose from 'mongoose';
+import { IComment } from '../interfaces/Icomment';
+import postModel from '../models/postModel';
+import userModel from '../models/userModel';
 
+export const CreateNewPost = async (
+  newPost: any,
+  author: string
+) => {
+  try {
+    const { title, content } = newPost;
 
-// export const CreateNewPost = async(newPost:any,id:string) => {
-//     try {
-//         const {title,content,author} = newPost
-//         const dbpost = new postModel({
-//             title,
-//             content,
-//             author:id
-//         })      
-//         await dbpost.save() 
-//         await userModel.updateOne(
-//             { _id: author },
-//             { $addToSet: { posts: dbpost._id } })
-//         return dbpost            
-//     } catch (err) {
-//         throw err
-//     }
-// }
-// export const CreateNewPost = async (newPost: any, id: string) => {
-//     try {
-//         const userId =  new mongoose.Types.ObjectId(id);
-//         const { title, content, author } = newPost;
-//         const dbpost = new postModel({
-//             title,
-//             content,
-//             author: userId
-//         });
+    const dbpost = new postModel({
+      title,
+      content,
+      author
+    });
 
-//         await dbpost.save();
-//         console.log("Post saved:", dbpost);
+    await dbpost.save();
 
-//         const updateResult = await userModel.updateOne(
-//             { _id: author},
-//             { $addToSet: { posts: dbpost._id } }
-//         );
+    
+    const userId = new mongoose.Types.ObjectId(author);
 
-//         console.log("User updated:", updateResult);
-//         return dbpost;            
-//     } catch (err) {
-//         console.error("Error creating post:", err);
-//         throw err;
-//     }
-// }
+    const updateResult = await userModel.updateOne(
+      { _id: userId },
+      { $addToSet: { posts: dbpost._id } }
+    );
 
-
-export const CreateNewPost = async (newPost: any, author: string) => {
-    try {
-        const { title, content } = newPost;
-
-        const dbpost = new postModel({
-            title,
-            content,
-            author
-        });
-
-        await dbpost.save();
-
-        // המרת ה-ID לאובייקט של Mongoose
-        const userId = new mongoose.Types.ObjectId(author); 
-
-        const updateResult = await userModel.updateOne(
-            { _id: userId },
-            { $addToSet: { posts: dbpost._id } }
-        );
-
-        console.log("User update result:", updateResult);
-        if (updateResult.matchedCount === 0) {
-            console.warn("No user found with the given ID.");
-        }
-        if (updateResult.modifiedCount === 0) {
-            console.warn("No changes made to the user.");
-        }
-
-        return dbpost;            
-    } catch (err) {
-        console.error("Error creating post:", err);
-        throw err;
+    console.log('User update result:', updateResult);
+    if (updateResult.matchedCount === 0) {
+      console.warn('No user found with the given ID.');
     }
+    if (updateResult.modifiedCount === 0) {
+      console.warn('No changes made to the user.');
+    }
+
+    return dbpost;
+  } catch (err) {
+    console.error('Error creating post:', err);
+    throw err;
+  }
 };
 
-
-
-export const DeletePost = async(id:any) => {
-    try {
-        await postModel.deleteOne({_id: id})
-        await userModel.updateOne(
-            { posts: id },
-            { $pull: { posts: id } })
-            return {
-                message: "Post deleted successfully"
-            }
-    } catch (err) {
-        throw err
-    }
-}
-
-export const GetAllPost = async() => {
-    try {
-        const posts = await postModel.find({})
-        console.log(posts);
-        return posts        
-    } catch (err) {
-        throw err
-    }
-}
-
-export const GetPostById = async(id:any) => {
-    try {
-        const post = await postModel.findById({_id:id})
-        return post        
-    } catch (err) {
-        throw err
-    }
-}
-
-export const UpdatePost = async (id: string, new_update: any) => {
-    try {        
-        const my_object_update: { title?: string; content?: string } = {};
-
-        if (new_update.title) {
-        my_object_update.title = new_update.title;  
-        }
-
-        if (new_update.content) {
-        my_object_update.content = new_update.content; 
-        }
-        await postModel.findByIdAndUpdate(
-            id,{ $set: my_object_update });
-        return {
-            message: "Post updated successfully"
-        };  
-    }catch (err) {
-        throw err;
-    }
+export const DeletePost = async (id: any) => {
+  try {
+    await postModel.deleteOne({ _id: id });
+    await userModel.updateOne(
+      { posts: id },
+      { $pull: { posts: id } }
+    );
+    return {
+      message: 'Post deleted successfully'
+    };
+  } catch (err) {
+    throw err;
+  }
 };
 
+export const GetAllPost = async () => {
+  try {
+    const posts = await postModel.find({});
+    console.log(posts);
+    return posts;
+  } catch (err) {
+    throw err;
+  }
+};
 
-export const AddCommentForPost = async(id:any,idpost:any,respons:any) => {
-    try {
-        const { content } = respons
-        const object ={
-            content,
-            author: id,
-            createdAt: new Date()
-        }
-        console.log(object);
-        await postModel.updateOne(
-            { _id: idpost },           
-            {
-                $push: {
-                    comments: {
-                    $each:[]
-                  }
-                }
-              })
-            return {
-                message: "Comment added successfully"
-            }
-    } catch (err) {
-        throw err
+export const GetPostById = async (id: any) => {
+  try {
+    const post = await postModel.findById({ _id: id });
+    return post;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const UpdatePost = async (
+  id: string,
+  new_update: any
+) => {
+  try {
+    const my_object_update: {
+      title?: string;
+      content?: string;
+    } = {};
+
+    if (new_update.title) {
+      my_object_update.title = new_update.title;
     }
-}
+
+    if (new_update.content) {
+      my_object_update.content = new_update.content;
+    }
+    await postModel.findByIdAndUpdate(id, {
+      $set: my_object_update
+    });
+    return {
+      message: 'Post updated successfully'
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const AddCommentForPost = async (
+  id: any,
+  idpost: any,
+  respons: any
+) => {
+  try {
+    const { content } = respons;
+    const object = {
+      content,
+      author: id,
+      createdAt: new Date()
+    };
+    console.log(object);
+    await postModel.updateOne(
+      { _id: idpost },
+      {
+        $push: {
+          comments: {
+            $each: []
+          }
+        }
+      }
+    );
+    return {
+      message: 'Comment added successfully'
+    };
+  } catch (err) {
+    throw err;
+  }
+};
